7ff7151dba2f0a6a0525f1625be283853c6669f5
Rezking <73573770+Rezking@users.noreply.github.com>
2022-04-27 12:15:19 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -74,12 +74,6 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-}
 
 
 # Password validation
